f49bb82bd6eea26ad16485b146e6e5f44620729e
Juan Munguia <carlosmunguia23145@gmail.com>
2024-01-15 12:56:25 +0100

    pull request

---
+++ src/views/FiltersPageView.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Filters from '@/components/Filters.vue'; 
+</script>
+<template>
+    <div>
+        <Filters />
+    </div>
+</template>
+<style lang="scss" scoped>
+</style>
