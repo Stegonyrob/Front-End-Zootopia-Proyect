3db5c1bbf91bb3c8e5efb4dcb50d0aeb1f01b985
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-01-24 16:30:30 +0100

    add animal functional

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+import axios from 'axios';
+
+let nombre = '';
+let tipo = '';
+let familia = '';
+let origen = '';
+let date = '';
+let genero = '';
+let selectedImagePath = '';
+
+const submitForm = () => {
+ axios.post("http://localhost:8080/api/v1/animals", {
+    name: nombre,
+    type: tipo,
+    family: familia,
+    origin: origen,
+    date: date,
+    gener: genero,
+    image_path: selectedImagePath 
+ })
+ .then(response => {
+    console.log(response);
+ })
+ .catch(error => {
+    console.log(error);
+ });
+}
+</script>
+
+
+
+
 <template>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link href="" rel="stylesheet">
@@ -7,15 +40,15 @@
       <form action="" class="row">
         <div class="col-4 text-center p-2">
           <label for="nombre" class="form-label">Nombre:</label>
-          <input type="text" class="form-control" id="nombre">
+          <input type="text" class="form-control" id="nombre" v-model="nombre">
         </div>
         <div class="col-4 text-center p-2">
           <label for="tipo" class="form-label">Tipo:</label>
-          <input type="text" class="form-control" id="tipo">
+          <input type="text" class="form-control" id="tipo" v-model="tipo">
         </div>
         <div class="col-4 text-center p-2">
           <label for="familia" class="form-label">Familia:</label>
-          <input type="text" class="form-control" id="familia">
+          <input type="text" class="form-control" id="familia" v-model="familia">
         </div>
         <div class="col-4 p-2">
           <figure class="figure p-2">
@@ -25,26 +58,24 @@
         </div>
         <div class="col-4 text-center p-2">
           <label for="date" class="form-label">Fecha de ingreso:</label>
-          <input type="date" class="form-control" id="date">
+          <input type="date" class="form-control" id="date" v-model="date">
 
           <label for="origen" class="form-label p-2">Origen:</label>
-          <input type="text" class="form-control" id="origen">
+          <input type="text" class="form-control" id="origen" v-model="origen">
         </div>
         <div class="col-4 text-center p-2">
           <label for="genero" class="form-label">Genero:</label>
-          <input type="text" class="form-control" id="genero">
+          <input type="text" class="form-control" id="genero" v-model="genero">
         </div>
         <div class="col-6">
-          <button type="button" class="btn btn_subir btn-lg float-end">Subir</button>
+          <button type="button" class="btn btn_subir btn-lg float-end" @click="submitForm">Subir</button>
+
         </div>
         
       </form>
   </div>
 </template>
 
-<script setup lang="ts">
-  
-</script>
 
 <style scoped lang="scss">
 
