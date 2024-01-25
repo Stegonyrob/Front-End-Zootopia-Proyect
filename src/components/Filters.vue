c62157bd2f17bac40a44b78668efc68354440fb3
Juan Munguia <carlosmunguia23145@gmail.com>
2024-01-25 08:45:42 +0100

    filters finished

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,23 +1,56 @@
 <script setup lang="ts">
-let requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
+import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+import axios from 'axios';
+
+
+
+interface Animal {
+ id: number;
+ image_path: string;
+ name: string;
+ family: string;
+ gener: string;
+ origin: string;
+ date: string;
+ type: string;
+ title: string;
+}
+
+const allAnimals = ref<Animal[]>([]);
+const itemsPerPage = 4;
+const currentPage = ref(1);
+const pages = computed(() => Math.ceil(allAnimals.value.length / itemsPerPage));
+
+const router = useRouter();
+
+const changePage = (page: number) => {
+ if (page >= 1 && page <= pages.value) {
+   currentPage.value = page;
+ }
+}
+
+const fetchAnimals = async () => {
+ const response = await axios.get("http://localhost:8080/api/v1/animals");
+ allAnimals.value = response.data;
 };
 
-fetch("http://localhost:8080/api/v1/animals", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+fetchAnimals();
 
+
+const sendEditForm = (id: any) => {
+ router.push({ name: 'Editar', params: { id: id } });
+}
 
 </script>
 <template>
     <div>
         <aside>
-            <form action="">
+            <form @submit.prevent>
                 <label for="">
                     <p>Nombre</p>
-                    <input type="text" >
+                    <input type="text" />
                 </label>
                 <label for="">
                     <p>Tipo</p>
@@ -41,13 +74,45 @@ fetch("http://localhost:8080/api/v1/animals", requestOptions)
                 </label>
 
                 <div class="button_div">
-                    <button on-keyup="searchData">Buscar</button>
+                    <button>Buscar</button>
                 </div>
             </form>
         </aside>
         <section>
-            
-        </section>
+            <div class="animals">
+            <div class="animals-cards">
+            <div v-for="(animal, index) in allAnimals.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)"
+                :key="animal.id" class="animal-card">
+                <img :src="animal.image_path" :alt="animal.title">
+                <div class="info-card">
+                <h3>{{ animal.title }}</h3>
+                <h5>Familia: {{ animal.family }}</h5>
+                <h5>Genero: {{ animal.gener }}</h5>
+                <h5>Origen: {{ animal.origin }}</h5>
+                <h5>Fecha de Entrada: {{ animal.date }}</h5>
+                <h5>Nombre: {{ animal.name }}</h5>
+                <h5>Tipo: {{ animal.type }}</h5>
+                <button @click="sendEditForm(animal.id)"><i class="bi bi-pencil-square"></i></button>
+                </div>
+            </div>
+            </div>
+            <nav aria-label="page" class="page">
+            <ul class="pagination active pagination-lg justify-content-center" aria-current="page">
+                <li class="page-item" :class="{ disabled: currentPage === 1 }">
+                <button class="page-link" @click="changePage(currentPage - 1)" :disabled="currentPage === 1"><i
+                    class="bi bi-arrow-left"></i></button>
+                </li>
+                <li class="page-item" v-for="page in pages" :key="page">
+                <button class="page-link" @click="changePage(page)">{{ page }}</button>
+                </li>
+                <li class="page-item" :class="{ disabled: currentPage === pages }">
+                <button class="page-link" @click="changePage(currentPage + 1)" :disabled="currentPage === pages"><i
+                    class="bi bi-arrow-right"></i></button>
+                </li>
+            </ul>
+            </nav>
+        </div>
+                </section>
     </div>
 </template>
 <style lang="scss" scoped>
